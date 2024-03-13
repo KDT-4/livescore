9594dfe45be374903f834c00f6af9a2df8b93010
yang-dal <yuop1022@gmail.com>
2024-03-13 12:21:51 +0900

    시스템 설정 글자크기 조정 완료

--- theme.js
+++ theme.js
@@ -1,20 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
     const slider = document.getElementById('font-slider');
+    const elementsToResize = document.querySelectorAll('.slider-label, .screen-styles, .service-center, .use-terms');
     const body = document.body;
     const lightModeButton = document.querySelector('.light-mode-button');
     const darkModeButton = document.querySelector('.dark-mode-button');
     const lightMode = document.querySelector('.light-mode');
     const darkMode = document.querySelector('.dark-mode');
     const boxes = document.querySelectorAll('.box'); // 모든 박스 선택
+
+    const homeLink = document.getElementById('home_link');
+    if (home_link) { // homeLink가 실제로 존재하는지 확인
+        homeLink.addEventListener('click', function(event) {
+            event.preventDefault(); // 기본 이벤트 방지
+            window.location.href = 'index.html'; // 홈 페이지로 이동
+        });
+    }
   
     // 초기 글자 크기 설정
     const initialFontSize = 100; // 예: 100%
     body.style.fontSize = `${initialFontSize}%`;
+    elementsToResize.forEach(element => {
+      element.style.fontSize = `${initialFontSize}%`;
+    });
 
     // 글자 크기 조절
     slider.addEventListener('input', () => {
       const fontSize = slider.value;
       body.style.fontSize = `${fontSize}%`;
+      elementsToResize.forEach(element => {
+        element.style.fontSize = `${fontSize}%`;
+      });
     });
   
     // 라이트 모드 버튼 클릭 이벤트
